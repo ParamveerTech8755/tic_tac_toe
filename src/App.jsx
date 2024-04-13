8d0b974b8d0b6cd3dc9c0528b0ea04e95f1f7165
Paramveer Singh <paramveerpuru@gmail.com>
2024-04-13 10:21:25 +0530

    UI changes @1.0.1

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,51 @@ import Log from "./components/Log"
 import {WINNING_COMBINATIONS} from "./winning-combinations.js"
 import GameOver from "./components/GameOver"
 
+const PLAYERS = {
+  X: "Player 1",
+  O: "Player 2"
+}
+
 let gameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
 ]
 
-function App() {
-
-  const [players, setPlayers] = useState({
-    X: "Player 1",
-    O: "Player 2"
-  })
-  const [gameTurns, setGameTurns] = useState([])
-
+function updateGameBoard(gameTurns){
   if(gameTurns.length > 0){
     const turn = gameTurns[0]
     const {square: {row, col}, player} = turn
     gameBoard[row][col] = player
   }
-  let winner = undefined
-  let isDraw = false
+}
+
+function checkForGameOver(){
+  let winner = null
 
   for(const ele of WINNING_COMBINATIONS){
     const firstEle = gameBoard[ele[0].row][ele[0].column]
     const secondEle = gameBoard[ele[1].row][ele[1].column]
     const thirdEle = gameBoard[ele[2].row][ele[2].column]
-    // console.log(firstEle, secondEle, thirdEle)
 
-    if(firstEle && firstEle === secondEle && firstEle === thirdEle)
-      winner = firstEle
+    if(firstEle && firstEle === secondEle && firstEle === thirdEle){
+      return winner = firstEle
+    }
   }
+}
 
-    if(!winner && gameTurns.length === 9)
-      isDraw = true
+function App() {
+
+  const [players, setPlayers] = useState(PLAYERS)
+  const [gameTurns, setGameTurns] = useState([])
+
+  updateGameBoard(gameTurns)
+
+  let isDraw = false
+  const winner = checkForGameOver()  
+  if(!winner && gameTurns.length === 9)
+    isDraw = true
+
 
 
   function handleMove(row, col){
@@ -74,15 +85,15 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initName="Player 1" onNameEdit={handlePlayerNameChange}
+          <Player initName={PLAYERS.X} onNameEdit={handlePlayerNameChange}
             symbol="X" isActive={gameTurns.length == 0 ? true:gameTurns[0].player === 'O'} />
-          <Player initName="Player 2" onNameEdit={handlePlayerNameChange}
+          <Player initName={PLAYERS.O} onNameEdit={handlePlayerNameChange}
             symbol="O" isActive={gameTurns.length == 0 ? false:gameTurns[0].player === 'X'} />
         </ol>
         {(winner || isDraw) && <GameOver winner={players[winner]} onRematch={handleRestart}/>}
         <GameBoard onSelect={handleMove} board={gameBoard} />
       </div>
-      <Log turns={gameTurns} />
+      {gameTurns.length > 0 && <Log turns={gameTurns} />}
     </main>
   )
 }
